2ba1fad5cf189a4d3b22ae5c9b2f700d7320d908
autumnfish <517729329@qq.com>
2021-12-25 10:05:56 +0800

    项目准备-配置路径别名

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,22 @@
+import { Button, Tag } from 'antd-mobile'
 function Login() {
-  return <div>Login</div>
+  return (
+    <div>
+      <Tag color='default'>Default</Tag>
+      <Tag color='primary'>测试</Tag>
+      <Tag color='success'>Success</Tag>
+      <Tag color='warning'>Warning</Tag>
+      <Tag color='danger'>Danger</Tag>
+      <Button
+        onClick={() => {
+          console.log('么么哒!')
+        }}
+        color='primary'
+      >
+        Primary
+      </Button>
+    </div>
+  )
 }
 
 export default Login
