2657dccffa84d35fe61ad115bbf51c75367496f6
autumnfish <517729329@qq.com>
2021-12-25 09:07:20 +0800

    项目准备-基础路由整合

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import './App.scss'
+// 路由
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Layout from './pages/Layout/index'
+import Login from './pages/Login/index'
 
 function App() {
-  return <div className='app'>app</div>
+  return (
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route path='/home' component={Layout}></Route>
+          <Route path='/login' component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
