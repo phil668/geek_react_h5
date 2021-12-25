d662cb89ac822a05df6e71642d455c3e1f8e5c68
autumnfish <517729329@qq.com>
2021-12-25 09:31:11 +0800

    项目准备-默认展示首页内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss'
 // 路由
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Layout from './pages/Layout/index'
 import Login from './pages/Login/index'
 
@@ -9,6 +9,13 @@ function App() {
     <Router>
       <div className='app'>
         <Switch>
+          <Route
+            exact
+            path='/'
+            render={() => {
+              return <Redirect to='/home' />
+            }}
+          ></Route>
           <Route path='/home' component={Layout}></Route>
           <Route path='/login' component={Login}></Route>
         </Switch>
