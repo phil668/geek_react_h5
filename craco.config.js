a582929c22fb09a22a5fb8a2580cdccf0b8b0d69
autumnfish <517729329@qq.com>
2021-12-25 11:55:16 +0800

    项目准备-增加路径别名

--- craco.config.js
+++ craco.config.js
@@ -14,6 +14,7 @@ module.exports = {
     alias: {
       // 约定：使用 @ 表示 src 文件所在路径
       '@': path.resolve(__dirname, 'src'),
+      '@scss': path.resolve(__dirname, 'src', 'assets', 'style'),
     },
   },
 
